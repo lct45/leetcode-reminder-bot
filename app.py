86c1f3770cdde3e673458afa77c87bcd2f548f1a
Andrew Li <ali@oberlin.edu>
2019-09-19 16:13:47 -0400

    Clean up loading env variables

--- app.py
+++ app.py
@@ -5,11 +5,17 @@ import yaml
 The follow variables must be specified in secret:
 CLIENT_ACCESS_TOKEN: Client access token from DialogFlow
 PAGE_ACCESS_TOKEN: Facebook page access token
-VERIFY_TOKEN: Verification token for Facebook chatbot
+VERIFICATION_TOKEN: Verification token for Facebook chatbot
 '''
-secretFile = open("secret.yaml")
-secretDict = yaml.load(secretFile,  Loader=yaml.FullLoader)
-secretFile.close()
+with open("secret.yaml") as secretFile:
+    secretDict = yaml.load(secretFile,  Loader=yaml.BaseLoader)
+    CLIENT_ACCESS_TOKEN =  secretDict["CLIENT_ACCESS_TOKEN"]
+    PAGE_ACCESS_TOKEN = secretDict["PAGE_ACCESS_TOKEN"]
+    VERIFICATION_TOKEN = secretDict["VERIFICATION_TOKEN"]
+
+
+
+
 
 
 
