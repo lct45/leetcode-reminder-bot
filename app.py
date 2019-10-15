5e0b094d43198c5a1eae2a881eb2abc975fc0543
Andrew Li <ali@oberlin.edu>
2019-10-14 21:59:27 -0400

    First time user behavior

--- app.py
+++ app.py
@@ -19,6 +19,22 @@ with open("secret.yaml") as secretFile:
 bot = Bot(PAGE_ACCESS_TOKEN) # PyMessenger Bot
 app = Flask(__name__) # Flask app
 
+# Sent if first time user is using bot
+greetings =  {"greeting":[ #Greetings 
+        {
+        "locale":"default",
+        "text":"Hello! We're going to make a 10xer out of you, {{user_first_name}}!"
+        }
+    ]}
+bot.set_greetings(greetings)
+gs = { #Get started button
+            "get_started":{
+            "payload":"start"
+            }
+    }
+bot.set_get_started(gs)
+
+
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=["GET", "POST"])
 def receive_message():
@@ -31,14 +47,27 @@ def receive_message():
           messaging = event["messaging"]
           for message in messaging:
             if message.get("message"):
-                #Facebook Messenger ID for user so we know where to send response back to
-                recipient_id = message["sender"]["id"]
-                if message["message"].get("text"):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
+                received_text(message)
+            elif message.get("postback"):
+                received_postback(message)
 
     return "Message Processed"
 
+def received_text(event):
+    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
+    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    text = event["message"]["text"]
+    
+    bot.send_text_message(recipient_id, "yeet")
+
+def received_postback(event):
+    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
+    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    payload = event["postback"]["payload"]
+    
+    if payload=='start':
+        send_message(sender_id, "What's up?")
+        
 
 def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
@@ -58,6 +87,8 @@ def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
+
+    
 
 if __name__ == "__main__":
     app.run()
--- pymessenger/bot.py
+++ pymessenger/bot.py
@@ -303,6 +303,29 @@ class Bot:
         """ Deprecated, use send_raw instead """
         return self.send_raw(payload)
 
+    def set_greetings(self, gs_obj):
+        """Set a get started button shown on welcome screen for first time users
+            https://developers.facebook.com/docs/messenger-platform/reference/messenger-profile-api/get-started-button
+            Input:
+              gs_obj: Your formatted get_started object as described by the API docs
+            Output:
+              Response from API as <dict>
+                            "greeting":[
+              {
+                "locale":"default",
+                "text":"Hello {{user_first_name}}!"
+              }
+            ]
+        """
+        request_endpoint = '{0}/me/messenger_profile'.format(self.graph_url)
+        response = requests.post(
+            request_endpoint,
+            params = self.auth_args,
+            json = gs_obj
+        )
+        result = response.json()
+        return result
+
     def set_get_started(self, gs_obj):
         """Set a get started button shown on welcome screen for first time users
         https://developers.facebook.com/docs/messenger-platform/reference/messenger-profile-api/get-started-button
