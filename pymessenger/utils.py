27768f256213f451be442126fe755a820a141465
Andrew Li <andruwuli@gmail.com>
2020-01-06 21:59:47 -0800

    Remove legacy Python 2 code from pymessenger

--- pymessenger/utils.py
+++ pymessenger/utils.py
@@ -1,6 +1,5 @@
 import hashlib
 import hmac
-import six
 
 
 def validate_hub_signature(app_secret, request_payload, hub_signature_header):
@@ -18,7 +17,8 @@ def validate_hub_signature(app_secret, request_payload, hub_signature_header):
         pass
     else:
         digest_module = getattr(hashlib, hash_method)
-        hmac_object = hmac.new(str(app_secret), unicode(request_payload), digest_module)
+        hmac_object = hmac.new(str(app_secret), unicode(
+            request_payload), digest_module)
         generated_hash = hmac_object.hexdigest()
         if hub_signature == generated_hash:
             return True
@@ -34,9 +34,8 @@ def generate_appsecret_proof(access_token, app_secret):
             appsecret_proof: HMAC-SHA256 hash of page access token
                 using app_secret as the key
     """
-    if six.PY2:
-        hmac_object = hmac.new(str(app_secret), unicode(access_token), hashlib.sha256)
-    else:
-        hmac_object = hmac.new(bytearray(app_secret, 'utf8'), str(access_token).encode('utf8'), hashlib.sha256)
+
+    hmac_object = hmac.new(bytearray(app_secret, 'utf8'), str(
+        access_token).encode('utf8'), hashlib.sha256)
     generated_hash = hmac_object.hexdigest()
     return generated_hash
